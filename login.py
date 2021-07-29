f76d400b83305762e509873efdeb3a2c99f6b71d
ExpertiseCyberInsights <88106617+ExpertiseCyberInsights@users.noreply.github.com>
2021-07-29 10:54:06 +0530

    Update login.py

--- login.py
+++ login.py
@@ -19,54 +19,5 @@ $$ |  $$\ $$ |  $$ |$$ |  $$ |$$   ____|$$ |      $$ |  $$\ $$ |$$  __$$ |$$ |  
           \$$$$$$  |                                                                
            \______/                                                                 
                                    ==================Created By Farhan==============""")
- username = 'user'
-password = 'pass'
  
-def restart():
-	ngulang = sys.executable
-	os.execl(ngulang, ngulang, *sys.argv)
  
-def main():
-	uname = raw_input("username : ")
-	if uname == username:
-		pwd = raw_input("password : ")
-		os.system('clear')
-		print('')
- 
-		if pwd == password:
-                        print ("\033[32;1m")
-                        print("\033[1;32;40m You have log in successfully\n")
-                        print ("\033[35;1m")
-                        os.system("date")
-                        print ("\033[36;1m_________________________________________\033[37;1m>")
-                        print ("\033[36;1m| \033[34;1mAuthor   \033[35;1m: \033[32;1mMr. Ism1 ")
-                        print ("\033[36;1m| \033[34;1mTEAM     \033[35;1m: \033[32;1mMUSLIM CYBER ARMY COMUNITI ")
-                        print ("\033[36;1m| \033[34;1mContack  \033[35;1m: \033[32;1mFacebook \033[35;1m$\033[37;1mAkhy Abdillah Tampanz ")
-                        print ("\033[36;1m| \033[34;1mE-Mail   \033[35;1m: \033[32;1moppocybera39@gmail.com ")
-                        print ("\033[36;1m|________________________________________\033[37;1m>")
-			print ("\033[31;1m[~]\033[33;1m============================================\033[31;1m[~]")
-			print ("\033[33;1m<----------[ \033[36;1mWellcome To My Terminal \033[33;1m]----------->")
-			print ("\033[31;1m[~]\033[33;1m============================================\033[31;1m[~]")
-			sys.exit()
- 
-		else:
-                        print ("\033[35;1m")
-			print ("Password Yang Anda Masukkan Salah")
-                        print ("\033[34;1mLogin Kembali")
-                        time.sleep(2)
-                        os.system('clear')
-			restart()
- 
-	else:
-                print ("\033[35;1m")
-		print ("Username yang Anda Masukkan Salah")
-                print ("\033[34;1mLogin Kembali")
-                time.sleep(2)
-                os.system('clear')
-		restart()
- 
-try:
-	main()
-except KeyboardInterrupt:
-	os.system('clear')
-	restart()
